4260f9714c090797d4f14d2ddb6f1cad5cd658fc
Kaio Dantas Mancini <dantaskaio01@gmail.com>
2026-01-17 19:56:20 -0300

    initial commit

---
+++ src/test/java/com/study/workshop/WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
