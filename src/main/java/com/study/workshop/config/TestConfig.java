a0f49a12ec6a64c2d602c3a105d507b1f3a03eac
Kaio Dantas Mancini <dantaskaio01@gmail.com>
2026-01-20 20:14:18 -0300

    create Enum OrderStatus

--- src/main/java/com/study/workshop/config/TestConfig.java
+++ src/main/java/com/study/workshop/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.study.workshop.entities.Order;
 import com.study.workshop.entities.User;
+import com.study.workshop.entities.enums.OrderStatus;
 import com.study.workshop.repositories.OrderRepository;
 import com.study.workshop.repositories.UserRepository;
 
@@ -27,8 +28,8 @@ public class TestConfig implements CommandLineRunner{
 		User firstUser = new User(null, "Kaio Mancini", "kaio@teste.com", "11999999999", "12345");
 		User secondtUser = new User(null, "Leticia Mancini", "leticia@teste.com", "11999999999", "12345");
 		
-		Order firstOrder = new Order(null, Instant.parse("2026-01-20T19:20:15Z"), firstUser);
-		Order secondOrder = new Order(null, Instant.parse("2026-01-13T10:20:15Z"), secondtUser);
+		Order firstOrder = new Order(null, Instant.parse("2026-01-20T19:20:15Z"), OrderStatus.SHIPPED , firstUser);
+		Order secondOrder = new Order(null, Instant.parse("2026-01-13T10:20:15Z"), OrderStatus.PAID, secondtUser);
 		
 		userRepository.saveAll(Arrays.asList(firstUser, secondtUser));
 		orderRepository.saveAll(Arrays.asList(firstOrder, secondOrder));
